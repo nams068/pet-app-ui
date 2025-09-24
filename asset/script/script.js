08b8d0653d0c48949ea46521e89033162598cb52
nams068 <karacaoglan1119@gmail.com>
2025-09-24 21:46:07 +0300

    Initial commit

---
+++ asset/script/script.js
@@ -0,0 +1,36 @@
+const slider = document.querySelector('.appointment-cards-wrapper');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', (e) => {
+  isDown = true;
+  slider.classList.add('active'); // opsiyonel, stil için
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener('mouseleave', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+
+slider.addEventListener('mouseup', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+
+slider.addEventListener('mousemove', (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 2; // kaydırma hızı
+  slider.scrollLeft = scrollLeft - walk;
+});
+document.querySelector('.profile').addEventListener('click', function() {
+  window.location.href = "Profile.html";
+});
+
+
+
+
